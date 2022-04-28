782720e7f0dba3aff781c60d1352bbed24afa3e1
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-04-28 13:37:00 +0300

    Header ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Register from "../Register/Register";
 import Login from "../Login/Login";
 import NotFound from "../NotFound/NotFound";
+import Navigation from "../Navigation/Navigation";
+import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <NotFound />
+      <Header/>
     </div>
   );
 }

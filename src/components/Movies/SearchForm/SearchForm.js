3f2f1196dc567b55ac04e2f3b862cc4cb865ee86
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-06-02 16:14:10 +0300

    all fixed v1.1.1

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -46,7 +46,7 @@ function SearchForm({ search, inputKeyString }) {
 
         <button
           type='submit'
-          className='search-form__button'>
+          className='search-form__button'
           onClick={handleSearch}>
           <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
           <rect width="34" height="34" rx="17" fill="#2BE080"/>
